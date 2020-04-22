17b266704f5b5a8f92d281d39add685e5bbec59c
fwpushan <pushan@freshworks.io>
2020-04-23 02:48:53 +0530

    Add entry option vc

---
+++ InvasivesBC/ViewControllers/LandingPage/EntryOptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryOptionViewController.swift
+//  InvasivesBC
+//
+//  Created by Pushan  on 2020-04-23.
+//  Copyright © 2020 Amir Shayegh. All rights reserved.
+//
+
+import UIKit
+
+class EntryOptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
